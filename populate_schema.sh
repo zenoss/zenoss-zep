60b1cb62b3443824059db50eba4f2f4851904b83
pwarren <pwarren@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-04-22 15:01:45 +0000

    Add support for ZEP schema migration. Reviewed by ian.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zep@41726 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- populate_schema.sh
+++ populate_schema.sh
@@ -1,70 +1,11 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2010, Zenoss Inc.  All Rights Reserved.
+# Copyright (C) 2010-2011, Zenoss Inc.  All Rights Reserved.
 #
-
-DBNAME=${DBNAME:-"zenoss_zep"}
-DBHOST=${DBHOST:-"localhost"}
-DBUSER=${DBUSER:-"root"}
-PROMPTPASS=0
-FORCE=0
-
-usage() {
-    echo "Usage: $(basename "$0") [...]"
-    echo ""
-    echo "  --dbhost <dbhost> (default '$DBHOST')"
-    echo "  --dbname <dbname> (default '$DBNAME')"
-    echo "  --dbuser <dbuser> (default '$DBUSER')"
-    echo "  --promptpass      (prompts for database password)"
-    echo "  --force           (overwrites existing database)"
-    echo ""
-    exit 0
-}
-
-while [ $# -gt 0 ]; do
-    ARG=$1
-    shift
-    case "$ARG" in
-        '--dbname')
-            DBNAME="$1"
-            shift
-            ;;
-        '--dbhost')
-            DBHOST="$1"
-            shift
-            ;;
-        '--dbuser')
-            DBUSER="$1"
-            shift
-            ;;
-        '--promptpass')
-            PROMPTPASS=1
-            ;;
-        '--force')
-            FORCE=1
-            ;;
-        *)
-            usage
-    esac
-done
 
 BASEDIR=`dirname "$0"`
 BASEDIR=`cd "$BASEDIR" && pwd`
 
-MYSQL="mysql --user=$DBUSER --host=$DBHOST"
-if [ $PROMPTPASS -gt 0 ]; then
-    MYSQL="$MYSQL --password"
-fi
-
-
-if [ $FORCE -gt 0 ]; then
-    DROP_DB="drop database if exists $DBNAME;"
-fi
-
-exec $MYSQL <<EOF
-$DROP_DB create database $DBNAME;
-use $DBNAME;
-source $BASEDIR/core/src/main/sql/event_schema.sql
-EOF
-RC=$?
+SCHEMADIR="$BASEDIR/core/src/main/sql/mysql"
+exec "$BASEDIR/dist/src/assembly/bin/zenoss-zep-create-db" --schemadir "$SCHEMADIR" "$@"
