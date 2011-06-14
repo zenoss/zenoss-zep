85843bfa20c2e834c76fc4e41c20c07ee40053c3
pwarren <pwarren@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-06-14 20:39:29 +0000

    Fix issue where schema_version wasn't properly set on last change. Reviewed by jhanson.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zep@44092 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- core/src/main/sql/mysql/002.sql
+++ core/src/main/sql/mysql/002.sql
@@ -5,3 +5,5 @@ SET storage_engine=InnoDB;
 -- DROP unused columns from event_archive
 ALTER TABLE `event_archive` DROP COLUMN `fingerprint_hash`;
 ALTER TABLE `event_archive` DROP COLUMN `clear_fingerprint_hash`;
+
+INSERT INTO `schema_version` (`version`, `installed_time`) VALUES(2, NOW());
