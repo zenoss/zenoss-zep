68ab0883fa19b9c29993da619310969e7711dda0
pwarren <pwarren@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-08-11 19:54:01 +0000

    Default sent_signal to 1 for existing spool entries. Reviewed by jplouis. Fixes #28828.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zep@46495 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- core/src/main/sql/mysql/002.sql
+++ core/src/main/sql/mysql/002.sql
@@ -4,4 +4,7 @@ SET storage_engine=InnoDB;
 
 ALTER TABLE `event_trigger_signal_spool` ADD COLUMN `sent_signal` TINYINT DEFAULT 0 NOT NULL;
 
+-- Set initial state on sent_signal to true for existing spool entries
+UPDATE `event_trigger_signal_spool` SET sent_signal=1;
+
 INSERT INTO `schema_version` (`version`, `installed_time`) VALUES(2, NOW());
