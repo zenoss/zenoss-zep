9b3b0c2aeb36c3ea37eb83810d05600d1b0cdbac
Paco Ibanez-Castillo <fraibacas@gmail.com>
2018-10-05 09:40:09 -0500

    add copyright header

--- core/src/main/java/org/zenoss/zep/zing/impl/ZingEmulatorPublisherImpl.java
+++ core/src/main/java/org/zenoss/zep/zing/impl/ZingEmulatorPublisherImpl.java
@@ -1,3 +1,12 @@
+/*****************************************************************************
+ *
+ * Copyright (C) Zenoss, Inc. 2018, all rights reserved.
+ *
+ * This content is made available according to terms specified in
+ * License.zenoss under the directory where your Zenoss product is installed.
+ *
+ ****************************************************************************/
+
 package org.zenoss.zep.zing.impl;
 
 import com.google.api.gax.core.CredentialsProvider;
