a51f6b58d3dfe5cccbf65e62c8c586f42242715d
Oleksandr Styhar <ostyhar@zenoss.com>
2019-07-16 06:54:24 +0000

    ZEN-31848: ZEP should limit message sizes sent to pubsub

--- core/src/main/java/org/zenoss/zep/zing/ZingPublisher.java
+++ core/src/main/java/org/zenoss/zep/zing/ZingPublisher.java
@@ -96,15 +96,23 @@ public abstract class ZingPublisher {
                 zingEvent.getTimestamp();
     }
 
+    public void incFailedEventsCounter() {
+        this.failedEventsCounter.inc();
+    }
+
+    public PubsubMessage getPubSubMessage(ZingEvent event) {
+        final Event zingEvent = event.toZingEventProto();
+        final ByteString bytes = zingEvent.toByteString();
+        return PubsubMessage.newBuilder()
+            .setData(bytes)
+            .putAttributes(ZingConstants.PUBSUB_ID_ATTRIBUTE, getMessageId(zingEvent))
+            .build();
+    }    
+
     public void publishEvent(ZingEvent event) {
         if (this.publisher != null) {
-            final Event zingEvent = event.toZingEventProto();
-            final ByteString bytes = zingEvent.toByteString();
-            this.bytesSentCounter.inc(bytes.size());
-            PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
-                    .setData(bytes)
-                    .putAttributes(ZingConstants.PUBSUB_ID_ATTRIBUTE, getMessageId(zingEvent))
-                    .build();
+            PubsubMessage pubsubMessage = getPubSubMessage(event);
+            this.bytesSentCounter.inc(pubsubMessage.getData().size());
             ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
             ApiFutures.addCallback(messageIdFuture, new ApiFutureCallback<String>() {
                 @Override
